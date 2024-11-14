8f8c89adb0635a54d1324a6ff333e40b55024ec0
James Kuchle <jkuchle@cmitcincy.com>
2024-11-14 16:52:10 -0500

    Made changes to get all licensed users.

--- add-userstoprotect.ps1
+++ add-userstoprotect.ps1
@@ -1,13 +1,14 @@
-﻿#This is a script that will pull a list of users that have Business Premium or E5 licenses, then add them to the list of Targeted Users to protect for the Standard Preset Security Policy
-#connect to MSOL so list licenses
+﻿#This is a script that will pull a list of users, then add them to the Impersonation detection policy. It will prompt you to log in twice as Client's global admin. 
+Install-Module MSOnline
+Install-Module ExchangeOnlineManagement
+Import-Module ExchangeOnlineManagement
+#connect to Microsoft Online Service to list users, Exchange Online to change antiphish policy
 Connect-Msolservice
 Connect-ExchangeOnline
 #get premium users
-$user_list = Get-MsolUser | Where-Object {($_.licenses).AccountSkuId -match "SPB" -or ($_.licenses).AccountSkuID -match "E5"}
-#connect to EO to change antiphish policy
+$user_list = Get-MsolUser | Where-Object -Property isLicensed -eq True
 #format the user list to match what antiphish policy requires for the TargetedUsersToProtect Field
 $formatted_ul = $user_list | ForEach-Object { $_.DisplayName + ";" + $_.UserPrincipalName}
-$policy = get-antiphishpolicy | Where-Object -Property Identity -Match "Standard"
-$policyID = $policy.Identity
+$policyID = (get-antiphishpolicy | Where-Object -Property Identity -Match "Standard").Identity
 set-antiphishpolicy -Identity $policyID -TargetedUsersToProtect $formatted_ul
-#extra comment just to test github
+Get-AntiPhishPolicy -Identity $policyID
